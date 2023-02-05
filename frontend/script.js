12b076e537a4e7cf0f12050a2f4aabe78aba2f62
Rebecca Ahn <rebeccahn360@gmail.com>
2023-02-05 04:23:11 -0800

    Connect backend and frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
-// Example: https://api.codetabs.com/v1/proxy?quest=https://c76b-128-195-97-145.ngrok.io/compliment/random
+// Update with proper ngrok link before running the website
 fetch("https://api.codetabs.com/v1/proxy?quest=https://paste-your-ngrok-link-here.ngrok.io/compliment/random")
     .then(res => res.json())
     .then(data => {
-        document.getElementById('display').innerHTML = data.compliment
+        document.getElementById('comp').innerHTML = data.compliment
     });
