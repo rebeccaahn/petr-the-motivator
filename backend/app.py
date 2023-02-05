7cab79bb2294ef98548962a860d926e32370bd37
Rebecca Ahn <rebeccahn360@gmail.com>
2023-02-05 03:39:47 -0800

    Updated compliments

--- backend/app.py
+++ backend/app.py
@@ -22,49 +22,49 @@ compliments = [
     {
         "compliment": "Your computer must be so powerful that I bet it runs C++ code in 1 second!",
         "category": categories[0],
-        "level": 8,     # Level of absurdity from 1 to 10 where 1 is the least absurd to 10 is the most absurd
+        "level": 5,     # Level of absurdity from 1 to 10 where 1 is the least absurd to 10 is the most absurd
         "short": "false"  # Short: <= 55 characters
     },
     {
         "compliment": "You're so pretty that I bet you can be the hottest celebrity in the whole wide world.",
         "category": categories[1],
-        "level": 7,
+        "level": 3,
         "short": "false"
     },
     {
-        "compliment": "You are so big brain that your brain must be the size of a yoga ball!",
-        "category": categories[2],
-        "level": 9,
+        "compliment": "You are so big brain that your brain must always be thinking of O(log n)",
+        "category": categories[0],
+        "level": 4,
         "short": "false"
     },
     {
-        "compliment": "That hat suits you so much that it should be a part of your hairstyle",
+        "compliment": "That outfit suits you so much that I think you should wear it every single day.",
         "category": categories[1],
-        "level": 6,
+        "level": 2,
         "short": "false"
     },
     {
         "compliment": "Your heart is as pure as water--clear and fresh",
         "category": categories[2],
-        "level": 4,
+        "level": 1,
         "short": "true"
     },
     {
         "compliment": "Your skin is so clean that I wonder if you're a doll",
         "category": categories[1],
-        "level": 5,
+        "level": 3,
         "short": "true"
     },
     {
         "compliment": "You shine so brightly that I think you must've fallen down from heaven",
         "category": categories[1],
-        "level": 8,
+        "level": 4,
         "short": "false"
     },
     {
         "compliment": "Your passion for coding is so hot that it makes me melt instantly",
         "category": categories[0],
-        "level": 9,
+        "level": 5,
         "short": "false"
     },
 ]
@@ -106,8 +106,8 @@ def get_list_of_filtered_compliments(category: str, level: int, short: str):
             if comp["category"] != category:
                 categorySet.remove(comp["compliment"])
         if level != None:
-            if level < 1 or level > 10:
-                abort(404, description="Invalid level. Level must be between 1 and 10 inclusive")
+            if level < 1 or level > 5:
+                abort(404, description="Invalid level. Level must be between 1 and 6 inclusive")
             if comp["level"] != level:
                 levelSet.remove(comp["compliment"])
         if short != None:
